2e492c01dc01841abbda4992b209034d8cc892a2
Nhat <nhaatn1@gmail.com>
2022-01-11 20:58:14 -0700

    sending a POST request to firebase

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,25 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetup() {
-  return <div>New Meetups Page</div>
+  function addMeetupHandler(meetupData) {
+    fetch(
+      "https://react-meetup-demo-13c07-default-rtdb.firebaseio.com/meetups.json",
+      {
+        method: "POST",
+        body: JSON.stringify(meetupData),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+  }
+
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
+    </section>
+  );
 }
 
 export default NewMeetup;
