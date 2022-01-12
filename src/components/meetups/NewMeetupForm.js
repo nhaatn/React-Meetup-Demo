9245d26535fae121395ec00c6cfe303fc598b109
Nhat <nhaatn1@gmail.com>
2022-01-11 19:42:20 -0700

    added form to new meetup page

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,30 @@
+import Card from "../ui/Card";
+import classes from './NewMeetupForm.module.css';
+
+function NewMeetupForm() {
+  return <Card>
+    <form className={classes.form}>
+      <div className={classes.control}>
+        <label htmlFor="title">Meetup Title</label>
+        <input type="text" id="title" required />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="image">Meetup Image</label>
+        <input type="url" id="image" required />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="address">Address</label>
+        <input type="text" id="address" required />
+      </div>
+      <div className={classes.control}>
+        <label htmlFor="description">Description</label>
+        <textarea id="description" rows="5" required></textarea>
+      </div>
+      <div className={classes.actions}>
+        <button>Add Meetup</button>
+      </div>
+    </form>
+  </Card>
+}
+
+export default NewMeetupForm;
